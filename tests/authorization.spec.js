573aa5f8d152ea955dda91a55b37221240dcf944
Elgrueber <elenagrueber@gmail.com>
2025-03-10 14:31:16 -0400

    rename test

--- tests/authorization.spec.js
+++ tests/authorization.spec.js
@@ -2,10 +2,11 @@ import { expect } from '@playwright/test';
 import { test } from '../fixtures';
 
 
-test('Verify SigninPage tite, verify Home Page title', async ({ page, homePage, signinPage }) => {
+test('Verify user login', async ({ page, homePage, signinPage }) => {
     await signinPage.navigate();
     const signinPageTitle = await signinPage.getTitle();
 
+    //verify that the Login page title is correct
     expect(signinPageTitle).toBe('TopCashback.com: Highest Cash Back Guaranteed');
 
     await signinPage.clickLoginLink();
@@ -16,6 +17,7 @@ test('Verify SigninPage tite, verify Home Page title', async ({ page, homePage, 
     await page.waitForTimeout(3000);
     const homePageTitle = await homePage.getTitle();
 
+    //verify that the Home page title is correct
     expect(homePageTitle).toBe("TopCashback Official Site - The USA's Most Generous Cash Back Site.");
 });
 
